20c4118cc8204e42148519c15fb1a3bca74e644b
vijayakumari <user622@host.docker.internal>
2023-10-26 11:36:22 +0530

    First_Example_Commit

---
+++ devops-demo/src/main/java/com/example/demo/controllers/WelcomeController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class WelcomeController {
+	
+	public String[] getCity() {
+		return new String[] {"Andhra","Tamilnadu","Telangana"};
+
+    }
+}
